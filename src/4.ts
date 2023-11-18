e343cc1ee411a8b95da61d6edebc33d622d80f20
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-11-18 16:31:14 +0200

    Up task 4;

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,80 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+// export {};
+
+// Клас Key
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Клас Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас House
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false; // По замовчуванню двері закриті
+  }
+
+  abstract openDoor(key: Key): void; 
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed. Cannot enter.");
+    }
+  }
+}
+
+// Клас MyHouse, який успадковується від House
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
+// Сценарій використання
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
